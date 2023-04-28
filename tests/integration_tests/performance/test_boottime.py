00c3522768fabd2cc066202f4a9f12ed06b1392e
Pablo Barbáchano <pablob@amazon.com>
2023-04-28 19:43:40 +0200

    chore: move find_log_message to its only user
    This is to keep Microvm trim
    
    Signed-off-by: Pablo Barbáchano <pablob@amazon.com>

--- tests/integration_tests/performance/test_boottime.py
+++ tests/integration_tests/performance/test_boottime.py
@@ -4,6 +4,7 @@
 
 import platform
 import re
+import time
 
 from framework.properties import global_props
 from framework.utils_cpuid import get_cpu_model_name, get_instance_type
@@ -111,8 +112,13 @@ def test_initrd_boottime(test_microvm_with_initrd, record_property, metrics):
 
 def _test_microvm_boottime(vm, max_time_us=MAX_BOOT_TIME_US):
     """Auxiliary function for asserting the expected boot time."""
-    boot_time_us = 0
-    timestamps = vm.find_log_message(TIMESTAMP_LOG_REGEX)
+    boot_time_us = None
+    timestamps = []
+    for _ in range(10):
+        timestamps = re.findall(TIMESTAMP_LOG_REGEX, vm.log_data)
+        if timestamps:
+            break
+        time.sleep(0.1)
     if timestamps:
         boot_time_us = int(timestamps[0])
 
