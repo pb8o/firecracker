pb8o/firecracker

commit a46b60e9ba774e831484315f20b2e437cc6c150f

ci: output disk usage if tests fail
Sometimes test fail because of running out of space. In that case it
would be helpful to dump some information on disk usage.

Signed-off-by: Pablo Barbáchano <pablob@amazon.com>

tools/test.sh
@@ -37,4 +37,14 @@ cp -ruvf build/img /srv
 cd tests
 export PYTEST_ADDOPTS="${PYTEST_ADDOPTS:-} --pdbcls=IPython.terminal.debugger:TerminalPdb"
 pytest "$@"
-exit $?
+ret=$?
+
+# if the tests failed and we are running in CI, print some disk usage stats
+# to help troubleshooting
+if [ $ret != 0 ] && [ "$BUILDKITE" == "true" ]; then
+    df -ih
+    df -h
+    du -h / 2>/dev/null |sort -h |tail -32
+fi
+
+exit $ret

commit 21d9d5af56de04100d6948792c1c94e4b3db4ede

fix(ci): ignore errors so we can capture the exit status
The rest of the script was not executed because we run the script under
`-e`. Disable errors so we can capture the exit code and print the
information.

Fixes: a46b60e9ba774e831484315f20b2e437cc6c150f

Signed-off-by: Pablo Barbáchano <pablob@amazon.com>

tools/test.sh
@@ -36,6 +36,7 @@ cp -ruvf build/img /srv
 
 cd tests
 export PYTEST_ADDOPTS="${PYTEST_ADDOPTS:-} --pdbcls=IPython.terminal.debugger:TerminalPdb"
+set +e
 pytest "$@"
 ret=$?
 